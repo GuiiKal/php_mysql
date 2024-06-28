9de1888858b1d3b8f01869435f27271063064525
guilhermekalcnd <gschenfeld@notredamecampinas.net.br>
2024-06-28 17:51:44 -0300

    database connection

---
+++ src/conection-bd.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+
+// Inicialização da conexão PDO
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=serenatto', 'root', 'notre123');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php/mysql',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e56e86940a59912e562e6c1d4f34b6cc4f02a092',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php/mysql' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e56e86940a59912e562e6c1d4f34b6cc4f02a092',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
