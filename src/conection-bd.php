d56a1d75a9056dac4381fed9932dbeecbe7504a8
Guilherme Kal <150172104+guilhermekalcnd@users.noreply.github.com>
2024-07-03 09:25:48 -0300

    Update conection-bd.php

--- src/conection-bd.php
+++ src/conection-bd.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Inicialização da conexão PDO
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=serenatto', 'root', 'notre123');
+    $pdo = new PDO('mysql:host=localhost;dbname=serenatto', 'root', '123');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage());
